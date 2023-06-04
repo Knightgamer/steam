f4f4b88a4d19af67605453b61b2c89c2eb8417e4
iWB-Git <sbp1784@gmail.com>
2023-06-05 01:40:06 +0300

    added search bar

--- steam/src/App.js
+++ steam/src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Trailer from "./components/Trailer";
+import Search from "./components/Search";
 
 function App() {
   return (
     <div>
-      {/* <Navbar /> */}
       <Trailer />
+      <div className="max-w-[360px] md:max-w-[640px] lg:max-w-[1240px] mx-auto mt-14">
+        <Search />
+      </div>
     </div>
   );
 }
