5ce93816468c5462f88e38d48132109025ce4186
iWB-Git <sbp1784@gmail.com>
2023-06-04 03:44:18 +0300

    made changes to navigation code

--- steam/src/components/Navbar.jsx
+++ steam/src/components/Navbar.jsx
@@ -27,23 +27,22 @@ const Navbar = () => {
         <li className="p-2">Login</li>
         <li className="p-2">Install Steam</li>
       </ul>
-      <div className="mt-5 block md:hidden" onClick={handleNav}>
-        {!nav ? <AiOutlineClose size={24} /> : <RiMenu3Fill size={24} />}
+      <div onClick={handleNav} className="mt-5 block md:hidden">
+        {nav ? <AiOutlineClose size={24} /> : <RiMenu3Fill size={24} />}
       </div>
-      <div
-        className="fixed w-[60%] top-0 mt-10 h-full border-l border-l-gray-900 bg-[#0A0A0A] ease-in-out duration-500"
-        style={{ right: !nav ? 0 : "-100%" }}
+
+      <ul
+        className="fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-700"
+        style={{ left: nav ? 0 : "-100%" }}
       >
-        <ul className="pt-5">
-          <li className="p-4">Store</li>
-          <li className="p-4">Community</li>
-          <li className="p-4">About</li>
-          <li className="p-4">Support</li>
-          <li className="p-4">Login</li>
-          <li className="p-4">Install Steam</li>
-          <AiOutlineGlobal className="ml-3.5 mt-3" size={18} />
-        </ul>
-      </div>
+        <li className="p-4">Store</li>
+        <li className="p-4">Community</li>
+        <li className="p-4">About</li>
+        <li className="p-4">Support</li>
+        <li className="p-4">Login</li>
+        <li className="p-4">Install Steam</li>
+        <AiOutlineGlobal className="ml-3.5 mt-3" size={18} />
+      </ul>
     </div>
   );
 };
