b72d42954c26f201ca3c771e38b02bd93ad17094
iWB-Git <sbp1784@gmail.com>
2023-06-07 02:44:05 +0300

    made some changes to trailer

--- steam/src/App.js
+++ steam/src/App.js
@@ -12,7 +12,7 @@ function App() {
       <div className="max-w-[360px] md:max-w-[640px] lg:max-w-[1240px] mx-auto mt-7 md:mt-10 lg:mt-14">
         <Search />
         <Section1 />
-        <Testing/>
+        {/* <Testing/> */}
       </div>
     </div>
   );
--- steam/src/components/Trailer.jsx
+++ steam/src/components/Trailer.jsx
@@ -39,46 +39,63 @@ const Trailer = () => {
   };
 
   return (
-     <div className="h-[40vh] md:h-[60vh] lg:h-[80vh] relative group">
-    <video
-      ref={videoRef}
-      src={videos[currentIndex].url}
-      className="w-full h-full rounded-2xl object-cover"
-      autoPlay
-      loop
-      muted
-    ></video>
-    {/* Left Arrow */}
-    <div className="hidden group-hover:block absolute top-1/2 left-5 transform -translate-y-1/2 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer" style={{ zIndex: 10 }}>
-      <BsChevronCompactLeft onClick={prevSlide} size={30} />
-    </div>
-    {/* Right Arrow */}
-    <div className="hidden group-hover:block absolute top-1/2 right-5 transform -translate-y-1/2 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer" style={{ zIndex: 10 }}>
-      <BsChevronCompactRight onClick={nextSlide} size={30} />
-    </div>
-    <div className="absolute w-full top-0 h-full flex items-end justify-start mb-1" style={{ zIndex: 5 }}>
-      <div className="lg:mb-10 md:mb-4 mb-1 md:ml-20 lg:ml-36 ml-10">
-        <h1 className="text-white lg:text-4xl text-[10px] md:text-2xl font-semi-bold">
-          Assassin's Creed Valhalla
-        </h1>
-        <p className="text-[#00aeff] lg:text-2xl text-[10px] md:text-xs font-medium">
-          $5.99
-        </p>
+    <div className="h-[40vh] md:h-[60vh] lg:h-[80vh] relative group">
+      <video
+        ref={videoRef}
+        src={videos[currentIndex].url}
+        className="brightness-75 opacity-100 w-full h-full rounded-2xl object-cover transition-transform duration-1000 ease-in-out"
+        autoPlay
+        loop
+        muted
+      ></video>
+      {/* Left Arrow */}
+      <div
+        className="hidden group-hover:block absolute top-1/2 left-5 transform -translate-y-1/2 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer"
+        style={{ zIndex: 10 }}
+      >
+        <BsChevronCompactLeft onClick={prevSlide} size={30} />
       </div>
+      {/* Right Arrow */}
+      <div
+        className="hidden group-hover:block absolute top-1/2 right-5 transform -translate-y-1/2 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer"
+        style={{ zIndex: 10 }}
+      >
+        <BsChevronCompactRight onClick={nextSlide} size={30} />
+      </div>
+      <div
+        className="absolute w-full top-0 h-full flex items-end justify-start mb-1"
+        style={{ zIndex: 5 }}
+      >
+        <div className="lg:mb-10 md:mb-4 mb-1 md:ml-20 lg:ml-36 ml-10">
+          <h1 className="text-white lg:text-4xl text-[10px] md:text-2xl font-semi-bold">
+            Assassin's Creed Valhalla
+          </h1>
+          <p className="text-[#00aeff] lg:text-2xl text-[10px] md:text-xs font-medium">
+            $5.99
+          </p>
+          <button
+            type="submit"
+            className="text-white mt-1 bg-blue-700 hover:bg-blue-800 focus:ring-1 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800
+  sm:text-base
+"
+          >
+            Buy Now
+          </button>
+        </div>
+      </div>
+      <div className="flex justify-center py-2">
+        {videos.map((video, index) => (
+          <div
+            key={index}
+            className={`h-1 w-10 mx-1 rounded-full ${
+              index === currentIndex ? "bg-white" : "bg-gray-300/50"
+            }`}
+          ></div>
+        ))}
+      </div>
+      <Navbar />
     </div>
-    <div className="flex justify-center py-2">
-      {videos.map((video, index) => (
-        <div
-          key={index}
-          className={`h-1 w-10 mx-1 rounded-full ${
-            index === currentIndex ? "bg-white" : "bg-gray-300/50"
-          }`}
-        ></div>
-      ))}
-    </div>
-    <Navbar />
-  </div>
-);
+  );
 };
 
 export default Trailer;
