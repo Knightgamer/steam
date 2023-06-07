a5205e89e4f965ada862b66a4803d84b85d3ab2c
iWB-Git <sbp1784@gmail.com>
2023-06-07 10:27:05 +0300

    added z-10 to nav

--- steam/src/components/Navbar.jsx
+++ steam/src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   };
 
   return (
-    <div className="text-white top-0 left-0 right-0 flex absolute justify-between mx-auto px-4 backdrop-filter backdrop-blur-sm bg-gradient-to-b  from-[rgba(0,0,0,0.95)] to-[rgba(0,0,0,0.4)] shadow-lg">
+    <div className="text-white top-0 left-0 right-0 flex absolute justify-between mx-auto px-4 backdrop-filter backdrop-blur-sm bg-gradient-to-b  from-[rgba(0,0,0,0.95)] to-[rgba(0,0,0,0.4)] shadow-lg z-10">
       <h1 className="flex mt-3.5">
         <FaSteam className="mr-3" size={24} />
         <span>STEAM</span>
